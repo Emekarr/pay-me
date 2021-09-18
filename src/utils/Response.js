32e33d818479dd2ba2a06a1e745d924f11c5f20a
Emekarr <aceanyanwu02@gmail.com>
2021-09-19 00:12:36 +0100

    built the custom response class

---
+++ src/utils/Response.js
@@ -0,0 +1,31 @@
+export default class Response {
+  #response;
+  #status_code;
+  #res;
+
+  constructor(message, data, success) {
+    this.#response = {
+      message: this.#format(message),
+      data: data || null,
+      success: success == null ? true : false,
+    };
+  }
+
+  #format(message) {
+    message.replace(
+      /(^\w|\s\w)(\S*)/g,
+      (_, m1, m2) => m1.toUpperCase() + m2.toLowerCase()
+    );
+    return message;
+  }
+
+  payload() {
+    return this.#response;
+  }
+
+  respond(status_code, res) {
+    this.#status_code = status_code;
+    this.#res = res;
+    this.#res.status(this.#status_code).send(this.#response);
+  }
+}
