53a6f0c5b1fa3e5800c60b2b926654658a409eea
Emekarr <aceanyanwu02@gmail.com>
2021-09-19 01:24:46 +0100

    added wildcard charcter for 404 errors

--- src/PayMeServer.js
+++ src/PayMeServer.js
@@ -2,6 +2,7 @@ import express from "express";
 import("./models/DBInstance.js");
 import error_middleware from "./middleware/error_middleware.js";
 import router from "./routes/index.js";
+import Response from "./utils/Response.js";
 
 class PayMeServer {
   constructor() {
@@ -13,6 +14,13 @@ class PayMeServer {
     this.express.use("/api", router);
 
     this.express.use(error_middleware);
+
+    this.express.use("*", (req, res, next) => {
+      new Response(`Route ${req.baseUrl} does not exist!`, false).respond(
+        404,
+        res
+      );
+    });
   }
 
   listen(port) {
