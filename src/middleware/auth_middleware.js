3b8b4c964c64c3fbe5c25f5a3b51d7c9bdca84c4
Emekarr <aceanyanwu02@gmail.com>
2021-09-30 02:05:00 +0100

    fixed bug with auth middleware

--- src/middleware/auth_middleware.js
+++ src/middleware/auth_middleware.js
@@ -2,24 +2,18 @@ import CustomError from "../utils/CustomError.js";
 
 export default (req, res, next) => {
   try {
-    try {
-      const authorization = req.get("Authorization");
-      if (!authorization) throw new CustomError("Authentication failed.", 400);
+    const authorization = req.get("Authorization");
+    if (!authorization) throw new CustomError("Authentication failed.", 400);
 
-      const token = authorization.split(" ")[1];
-      if (!token || token === " ")
-        throw new CustomError("Authentication failed.", 400);
+    const token = authorization.split(" ")[1];
+    if (!token || token === " ")
+      throw new CustomError("Authentication failed.", 400);
 
-      const result = jwt.verify(token, process.env.JWT_SECRET);
-      if (!result) throw new CustomError("Authentication failed.", 400);
+    const result = jwt.verify(token, process.env.JWT_SECRET);
+    if (!result) throw new CustomError("Authentication failed.", 400);
 
-      req.is_auth = true;
-      req.id = result.id;
-      next();
-    } catch (e) {
-      req.is_auth = false;
-      next();
-    }
+    req.id = result.id;
+    next();
   } catch (e) {
     next(e);
   }
